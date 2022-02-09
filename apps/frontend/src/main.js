53b9f47e0ebdff985c0a08076382df803d4223d2
aaa29 <aliane781@gmail.com>
2022-02-09 11:26:07 +0100

    feat: configure router and change pages structure

--- apps/frontend/src/main.js
+++ apps/frontend/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { VueQueryPlugin } from 'vue-query'
+import { createRouter, createWebHistory } from 'vue-router'
 
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/', component: () => import('./pages/Map.vue') },
+        { path: '/info', component: () => import('./pages/Info.vue') },
+    ],
+})
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueQueryPlugin)
+app.use(router)
+
+app.mount('#app')
